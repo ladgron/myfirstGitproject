a9307bfa7514bcaea3e62f1550cedc8bafc135ca
Ladan G <ladgron@gmail.com>
2020-10-21 11:35:21 +0200

    Hello GitHub

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
    System.out.println("my first Git project");
+        System.out.println("Hej Ladan");
     }
 }
